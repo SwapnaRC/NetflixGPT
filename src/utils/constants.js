5d26bc5f79ca877f94bab993a4db925fe99c2a77
SwapnaRC <chillal.swapnar@gmail.com>
2024-02-21 01:56:37 +0530

    formatted the file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,15 +4,16 @@ export const LOGO_URL =
 export const LOGIN_BACKGROUND_IMAGE =
   "https://assets.nflxext.com/ffe/siteui/vlv3/32c47234-8398-4a4f-a6b5-6803881d38bf/eed3a573-8db7-47ca-a2ce-b511e0350439/IN-en-20240122-popsignuptwoweeks-perspective_alpha_website_small.jpg";
 
-  export const PHOTO_URL = "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg";
+export const PHOTO_URL =
+  "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg";
 
-  export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MzkyYmNmMGQxYTY4YzVhYTQyYTFmOWRkOTg2M2E1NyIsInN1YiI6IjY1ZDA5OTEyZmQ2MzAwMDE4NmNhNGQ0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1HxFGybGicuTDxFkoAmCeKKSr8ySbP46Ax8GbfrFS6o'
-    }
-  };
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MzkyYmNmMGQxYTY4YzVhYTQyYTFmOWRkOTg2M2E1NyIsInN1YiI6IjY1ZDA5OTEyZmQ2MzAwMDE4NmNhNGQ0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1HxFGybGicuTDxFkoAmCeKKSr8ySbP46Ax8GbfrFS6o",
+  },
+};
 
-  export const IMAGE_URL= "https://image.tmdb.org/t/p/w500"
-  
+export const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
