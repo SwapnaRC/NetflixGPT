99db309e7f23aafb47ec9c0f688e479184192211
SwapnaRC <chillal.swapnar@gmail.com>
2024-02-23 02:17:33 +0530

    open ai api error

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 import { OPENAI_API_KEY  } from "./constants";
 
 const openai = new OpenAI({
-  organization: "org-ULuqtaAk1nwaN5Ym1EvUXA6N",
+  organization: "cant share bcz of privacy policy",
   apiKey: { OPENAI_API_KEY  },
   dangerouslyAllowBrowser: true,
 });
