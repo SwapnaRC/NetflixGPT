89a43a6eb6fb4d8b71fc025a1ae410d703e7e96f
SwapnaRC <chillal.swapnar@gmail.com>
2024-02-24 16:46:08 +0530

    netflix gpt results

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,22 +1,86 @@
-import React from "react";
+import React, { useRef } from "react";
 import lang from "../utils/languageConstant";
-import { useSelector } from "react-redux";
+import Error from "./Error";
+import { useDispatch, useSelector } from "react-redux";
+import genAI from "../utils/genai";
+import {
+  API_OPTIONS,
+  safetySettings,
+  generation_config,
+} from "../utils/constants";
+import { addGptMovieResults } from "../utils/gptSlice";
 
 const GptSearchBar = () => {
-  const langkey = useSelector((store)=> store.config.lang)
+  const dispatch = useDispatch();
+  const langkey = useSelector((store) => store.config.lang);
+  const searchText = useRef(null);
+
+  // search movie in TMDB
+  const searchMovieTMDB = async (movie) => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/search/movie?query=" +
+        movie +
+        "&include_adult=false&page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    return json.results;
+  };
+
+  const handleGptSearchClick = async () => {
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+    const prompt =
+      "Act as a movie recommendation system and suggest some movies for the query:" +
+      searchText.current.value +
+      ". Only give me names of 5 movies, comma seperated like the example result given ahead. Example Result Gadar, Sholay, Don, RRR, Kantara ";
+
+    const result = await model.generateContent(
+      prompt,
+      safetySettings,
+      generation_config
+    );
+
+    const response = await result.response;
+    if (!response) {
+      return <Error error={"API has some issue, its not responding"} />;
+    }
+    const text = response.text();
+    const searchMovieResults = text.split(",");
+    const promiseArray = searchMovieResults.map((movie) =>
+      searchMovieTMDB(movie)
+    );
+    const tmdbResults = await Promise.all(promiseArray);
+    dispatch(
+      addGptMovieResults({
+        movieNames: searchMovieResults,
+        movieResults: tmdbResults,
+      })
+    );
+  };
+
   return (
-    <div className="pt-[7%] flex justify-center">
-      <form className=" w-1/2 bg-black grid grid-cols-12">
-        <input
-          type="text"
-          className="p-4 m-4 col-span-9"
-          placeholder={lang[langkey].gptSearchPlaceholder}
-        />
-        <button className="py-2 px-4 col-span-3 m-4 bg-red-700 text-white rounded-lg ">
-          {lang[langkey].search}
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="pt-[7%] flex justify-center">
+        <form
+          className=" w-1/2 bg-black grid grid-cols-12"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <input
+            ref={searchText}
+            type="text"
+            className="p-4 m-4 col-span-9"
+            placeholder={lang[langkey].gptSearchPlaceholder}
+          />
+          <button
+            className="py-2 px-4 col-span-3 m-4 bg-red-700 text-white rounded-lg "
+            onClick={handleGptSearchClick}
+          >
+            {lang[langkey].search}
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
+import { HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
+
 export const LOGO_URL =
   "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
@@ -16,11 +18,37 @@ export const API_OPTIONS = {
   },
 };
 
-export const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+export const IMAGE_URL = "https://image.tmdb.org/t/p/w400";
 
 export const SUPPORTED_LANGUAGE = [
-  { identifier: "en", name: "English"},
-  { identifier: "hindi", name: "Hindi"},
-  { identifier: "kannada", name: "Kannada"},
-  { identifier: "spanish", name: "Spanish"},
-]
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "kannada", name: "Kannada" },
+  { identifier: "spanish", name: "Spanish" },
+];
+
+export const GEN_AI_API_KEY = "cant share bcz of privacy constant";
+export  const generation_config = {
+  "candidate_count": 1,
+  "max_output_tokens": 256,
+  "temperature": 1.0,
+  "top_p": 0.7,
+}
+export const safetySettings = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+  },
+];
